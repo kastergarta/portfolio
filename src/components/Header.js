8167fd10dc710aa844ce773eddd1420a25cc8348
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-04 22:46:32 -0400

    profile + contentful

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,17 +48,17 @@ const Header = () => (
           {({ allLinks }) => {
             const { home, links } = formatLinks(allLinks);
 
-            const homeLink = home && (
-              <Image
-                src={Logo}
-                width="50px"
-                alt="Portfolio Logo"
-                onClick={home.onClick}
-                style={{
-                  cursor: 'pointer',
-                }}
-              />
-            );
+            // const homeLink = home && (
+            //   <Image
+            //     src={Logo}
+            //     width="50px"
+            //     alt="Portfolio Logo"
+            //     onClick={home.onClick}
+            //     style={{
+            //       cursor: 'pointer',
+            //     }}
+            //   />
+            // );
             const navLinks = links.map(({ name, value }) => (
               <RouteLink
                 key={name}
@@ -70,7 +70,7 @@ const Header = () => (
 
             return (
               <Fragment>
-                {homeLink}
+                {/* {homeLink} */}
                 <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
               </Fragment>
             );
